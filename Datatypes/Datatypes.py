28e506f507dff7c050fc2b193b1cd0b00beb484d
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-24 20:18:25 +0600

    Add files via upload

---
+++ Datatypes/Datatypes.py
@@ -0,0 +1,22 @@
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+
+x=20
+
+x=float(20) #Specified dataTypes
+
+
+print(x)
+y= complex(2890)
+print(y)
+
+#,,,,,,,,,complex numbers.
+
+num1 = 2+8j
+num2 = 3+12j
+print(num1+num2)
