935cc780846084d7b7b976e77f5d90ac2a2a703c
RomonaSarkar16 <85759553+RomonaSarkar16@users.noreply.github.com>
2021-09-24 20:24:48 +0600

    Add files via upload

---
+++ List/List item access.py
@@ -0,0 +1,6 @@
+list = ["tomotes","apple","chicken","bread","mutton","butter",True, 500]
+some1 = input("enter the word :")
+if some1 in list:
+    print("yes, it is there baby girl")
+else:
+    print(" sorry it is not in the list")
---
+++ List/list copy.py
@@ -0,0 +1,9 @@
+#list item copy
+list = ["angela","sudha","ramisa"]
+list2 =[]
+
+print("blank: ",list2)
+list2 = list.copy()
+
+print("Copied list: ",list2)
+
