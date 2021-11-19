7d578388c0cf197c641844d17a51e6a8cb0dad29
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-11-19 19:05:48 +0600

    elif updated
    condition addded

--- List/List item access.py
+++ List/List item access.py
@@ -1,6 +1,9 @@
 list = ["tomotes","apple","chicken","bread","mutton","butter",True, 500]
+insert=input("Please insert a name : ")
 some1 = input("enter the word :")
 if some1 in list:
     print("yes, it is there baby girl")
+elif insert in list:
+    print("yes it is exist")
 else:
     print(" sorry it is not in the list")
